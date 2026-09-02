3898ac50049d714fc9270ebaffb0ff2174fe9537
chinthakanuwan <chinthaka36@gmail.com>
2026-09-02 08:25:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,61 @@
-const CACHE_NAME = 'foodhub-v1';
-const urlsToCache = ['./', './index.html', './app.js', './manifest.json'];
+const CACHE_NAME = 'foodhub-v3';  // ← v3 දැන්
+const urlsToCache = [
+    './',
+    './index.html?v=3',
+    './app.js?v=3',
+    './manifest.json?v=3'
+];
 
 self.addEventListener('install', event => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+    console.log('Service Worker installing...');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(response => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    const responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    return response;
+                });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activating...');
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => {
+            console.log('Service Worker activated');
+            return self.clients.claim();
+        })
+    );
 });
